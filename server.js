923fb4b697c19bb52d16840a7da56dc25a298df5
Abuzer Ozcan Kursun <ozcankursun@gmail.com>
2021-01-26 17:28:19 +0200

    Server started and listenning

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const { Client } = require("pg");
+const express = require("express");
+
+app = express();
 
 const client = new Client({
   user: "postgres",
@@ -6,19 +9,33 @@ const client = new Client({
   database: "eshop",
   password: "GE76V9EV",
   port: 5432,
-})
-client.connect();
-client
-  .query("SELECT * FROM manufacturer")
-  .then(function (results) {
-    console.log("Success!");
-    console.log(results.rowCount);
-    client.end();
-  })
-  .catch(function (results) {
-    console.log("Ooops!");
-    console.log(err);
-    client.end();
-  })
+});
+// client.connect();
+// client
+//   .query("SELECT * FROM manufacturer")
+//   .then( (results)=> {
+//     console.log("Success!");
+//     console.log(results.rowCount);
+//     client.end();
+//   })
+//   .catch( (results) => {
+//     console.log("Ooops!");
+//     console.log(err);
+//     client.end();
+//   });
+
+app.get("/", (req, resp) => {
+  resp.write("In Get");
+  resp.end();
+});
+
+app.get("/placeholder", (req, resp) => {
+  resp.write("placeholder");
+  resp.end();
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log("Server started and listenning at " + port);
+});
 
 console.log("Server is finishing");
