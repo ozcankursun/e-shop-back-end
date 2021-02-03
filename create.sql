4b036ca94794e805c64165f90c477d66a8446c8b
ozcankursun <62161820+ozcankursun@users.noreply.github.com>
2021-02-04 00:24:32 +0200

    adding constraints

--- create.sql
+++ create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE manufacturer (
 	id INT PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
-	country_code VARCHAR(100) NOT NULL UNIQUE,
+	country_code VARCHAR(100) NOT NULL,
 	link VARCHAR(100),
 	description VARCHAR(100),
 	more_description VARCHAR(100)
@@ -11,7 +11,7 @@ CREATE TABLE clothes (
 	id INT PRIMARY KEY,
 	code VARCHAR(50) NOT NULL UNIQUE,
 	image_filename VARCHAR(50) NOT NULL UNIQUE,
-	manufacturer_id VARCHAR(6) REFERENCES manufacturer ("id") ,
+	manufacturer_id INT REFERENCES manufacturer ("id") ,
 	short_description TEXT,
 	more_description TEXT
 );
@@ -19,7 +19,7 @@ CREATE TABLE clothes (
  CREATE TABLE orders (
 	id INT PRIMARY KEY,
 	order_date DATE NOT NULL,
-	cloth_id VARCHAR(6) REFERENCES clothes ("id"),
+	cloth_id INT REFERENCES clothes ("id"),
 	quantity INT NOT NULL,
 	customer_code VARCHAR(50) NOT NULL UNIQUE
 );
