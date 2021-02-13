26f3bf5fdc97638579fd1a6e81d731173f006a1e
ozcankursun <62161820+ozcankursun@users.noreply.github.com>
2021-02-13 21:22:19 +0200

    get methods of orders and clothes are added

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.post("/", (req, resp) => {
 
 app.get("/", (req, resp) => {
   let filterName = req.query.filterName;
-
+// text: "SELECT * FROM clothes"
   const myQuery = {
     text: "SELECT * FROM manufacturer WHERE name LIKE $1",
     values: ["%" + filterName + "%"],
@@ -115,11 +115,65 @@ app.get("/", (req, resp) => {
     });
 });
 
-
-app.get("/placeholder", (req, resp) => {
-  resp.write("placeholder");
-  resp.end();
+app.get("/clothes", (req, resp) => {
+  let filterName = req.query.filterName;
+// text: "SELECT * FROM clothes"
+  // const myQuery = {
+  //   text: "SELECT * FROM clothes",
+  //   values: ["%" + filterName + "%"],
+  // };
+  client
+    .query("SELECT * FROM clothes")
+    .then((results) => {
+      console.log("Success!");
+      console.log(results.rowCount);
+      resp.writeHead(200, {
+        "Content-Type": "text/json",
+      });
+      resp.write(JSON.stringify(results.rows));
+      resp.end();
+    })
+    .catch((error) => {
+      console.log("Ooops!");
+      console.log(error);
+      resp.writeHead(200, {
+        "Content-Type": "text/json",
+      });
+      resp.write(JSON.stringify("Failed"));
+      resp.end();
+    });
 });
+app.get("/orders", (req, resp) => {
+  let filterName = req.query.filterName;
+
+  client
+    .query("SELECT * FROM orders")
+    .then((results) => {
+      console.log("Success!");
+      console.log(results.rowCount);
+      resp.writeHead(200, {
+        "Content-Type": "text/json",
+      });
+      resp.write(JSON.stringify(results.rows));
+      resp.end();
+    })
+    .catch((error) => {
+      console.log("Ooops!");
+      console.log(error);
+      resp.writeHead(200, {
+        "Content-Type": "text/json",
+      });
+      resp.write(JSON.stringify("Failed"));
+      resp.end();
+    });
+});
+
+
+
+// app.get("/placeorder", (req, resp) => {
+//   resp.write("placehorder");
+//   resp.end();
+// });
 const port = 3000;
 app.listen(port, () => {
   console.log("Server started and listenning at " + port);
