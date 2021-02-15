ae66709334b018ae1280d07febfbc69248910d31
ozcankursun <62161820+ozcankursun@users.noreply.github.com>
2021-02-16 00:00:08 +0200

    update

--- server.js
+++ server.js
@@ -135,6 +135,36 @@ app.get("/clothes", (req, resp) => {
       resp.end();
     });
 });
+
+app.get("/cloth/orderable/:id",(req,resp)=>{
+  console.log("testing");
+  const myQuery = {
+      text: "SELECT * FROM clothes WHERE id=$1",
+      values: [req.params.id],
+    };
+    client
+      .query(myQuery)
+      .then((results) => {
+        console.log("Success!");
+        //console.log(results.rowCount);
+        resp.writeHead(200, {
+          "Content-Type": "text/json",
+        });
+        resp.write(JSON.stringify(results.rows[0]));
+        resp.end();
+      })
+      .catch((error) => {
+        console.log("Ooops!");
+        console.log(error);
+        resp.writeHead(200, {
+          "Content-Type": "text/json",
+        });
+        resp.write(JSON.stringify("Failed"));
+        resp.end();
+      });
+})
+
+
 app.get("/orders", (req, resp) => {
   //let filterName = req.query.filterName;
 
